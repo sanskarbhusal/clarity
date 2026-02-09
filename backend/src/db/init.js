b4ce80b7f9575fa7b455b7e27110bfb22d5cde6d
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-02-10 04:40:52 +0545

    feat/backend: add database seed and init logic

---
+++ backend/src/db/init.js
@@ -0,0 +1,26 @@
+import pool from "./pool.js"
+
+const initQuery = `
+CREATE TABLE
+  IF NOT EXISTS accounts (
+        email VARCHAR(255) PRIMARY KEY,
+        password_hash TEXT NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS transactions (
+        id SERIAL PRIMARY KEY,
+        email VARCHAR(255) REFERENCES accounts (email) ON DELETE CASCADE,
+        amount NUMERIC(10, 2) NOT NULL,
+        category VARCHAR(50) NOT NULL,
+        t_description TEXT,
+        t_date DATE NOT NULL
+    );
+    `
+
+try {
+    await pool.query(initQuery)
+    console.info("Database Initiated")
+} catch (err) {
+    console.error(err.message)
+}
---
+++ backend/src/db/seed.js
@@ -0,0 +1,39 @@
+import pool from "./pool.js"
+
+const seedQuery = `
+INSERT INTO
+    accounts (email, password_hash)
+VALUES
+    ('test@gmail.com', 'password');
+
+INSERT INTO
+    transactions (email, amount, category, t_description, t_date)
+VALUES
+    (
+        'test@gmail.com',
+        1000.00,
+        'investment',
+        'Invested in share market',
+        '2026-02-17'
+    ),
+    (
+        'test@gmail.com',
+        500.00,
+        'entertainment',
+        'Went to see movie',
+        '2026-02-18'
+    ),
+    (
+        'test@gmail.com',
+        4000.00,
+        'clothing',
+        'Bought new pant',
+        '2026-02-19'
+    );
+    `
+
+try {
+    await pool.query(seedQuery)
+} catch (err) {
+    console.err(err.message)
+} 
