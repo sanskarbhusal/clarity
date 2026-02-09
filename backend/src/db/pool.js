2e7454a3e23ffa32e0efd4c8202c9b315c550c27
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-02-10 04:39:15 +0545

    feat/backend: update pool.js Configure pg to ignore CA certificate while connecting to postgres server

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -9,6 +9,9 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool
