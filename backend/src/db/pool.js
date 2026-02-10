fd954b1096fa8b7dd2dcaf6f1d6785b5d697ab60
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-02-10 14:01:25 +0545

    feat/backend: add local and production sepearation in pool.js

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -3,15 +3,33 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: false
+let dbConfig
+
+if (process.env.ENVIRONMENT = 'production') {
+    dbConfig = {
+        host: process.env.DB_HOST_PRODUCTION,
+        user: process.env.DB_USER_PRODUCTION,
+        password: process.env.DB_PASSWORD_PRODUCTION,
+        database: process.env.DB_NAME_PRODUCTION,
+        port: process.env.DB_PORT_PRODUCTION,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
-});
+} else {
+    dbConfig = {
+        host: process.env.DB_HOST_LOCAL,
+        user: process.env.DB_USER_LOCAL,
+        password: process.env.DB_PASSWORD_LOCAL,
+        database: process.env.DB_NAME_LOCAL,
+        port: process.env.DB_PORT_LOCAL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+
+    }
+}
+
+const pool = new Pool(dbConfig);
 
 export default pool
