ee852ced0d6d32024a480e840030af9e6b81fa40
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-02-10 21:14:55 +0545

    feat/backend: update 'list' route controller logic

--- backend/src/controllers/transactionControllers.js
+++ backend/src/controllers/transactionControllers.js
@@ -5,7 +5,7 @@ async function getList(req, res) {
     let sql, values, result
 
     try {
-        sql = "SELECT amount, t_type, category, t_description, t_date FROM transactions WHERE email=$1;"
+        sql = "SELECT id, amount, t_type, category, t_description, t_date FROM transactions WHERE email=$1;"
         values = [email]
         result = await pool.query(sql, values)
         res.status(200).json(result.rows)
