eb575b5bfa6b734696d5856eedc025053d170e84
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-02-10 23:52:14 +0545

    feat/backend: add filter by category in getList route controller

--- backend/src/controllers/transactionControllers.js
+++ backend/src/controllers/transactionControllers.js
@@ -3,10 +3,17 @@ import pool from "../db/pool.js"
 async function getList(req, res) {
     const email = req.query.email
     let sql, values, result
+    const category = req.query.category
 
-    try {
+    if (category != undefined && category != "") {
+        sql = "SELECT id, amount, t_type, category, t_description, t_date FROM transactions WHERE email=$1 AND category=$2;"
+        values = [email, category]
+    } else {
         sql = "SELECT id, amount, t_type, category, t_description, t_date FROM transactions WHERE email=$1;"
         values = [email]
+    }
+
+    try {
         result = await pool.query(sql, values)
         res.status(200).json(result.rows)
     } catch (err) {
