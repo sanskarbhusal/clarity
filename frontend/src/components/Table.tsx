9b17c8eabead2f0529e9e15751d5169c54f00c21
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-02-11 20:48:22 +0545

    feat/frontend: add Pie Chart and Table component Just an initial template with mock data

---
+++ frontend/src/components/Table.tsx
@@ -0,0 +1,27 @@
+export default function Table() {
+    return (
+        <table>
+            <tr>
+                <th>Amount</th>
+                <th>Type</th>
+                <th>Category</th>
+                <th>Description</th>
+                <th>Date</th>
+            </tr>
+            <tr>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+            <tr>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+                <td>data</td>
+            </tr>
+        </table>
+    )
+}
