2e8d58e50352529156929ff91ecd433e86e9e047
Sanskar Bhusal <sanskarbhusal123@gmail.com>
2026-02-17 04:46:40 +0545

    feat/frontend: fix modal not closing issue while editin

--- frontend/src/components/EditTransactionModal.tsx
+++ frontend/src/components/EditTransactionModal.tsx
@@ -24,8 +24,6 @@ async function handleClick({ id, email, amount, t_type, category, t_description,
         // flip the previous state to trigger re-render
         setSyncTrigger((prev: boolean) => !prev)
 
-        closeModal()
-
     } catch (error) {
         const err = error as Error
         console.log(err.message)
@@ -45,85 +43,88 @@ export default function EditTransactionModal({ closeModal, data }: any) {
     const loggedInUser = useContext(AuthContext)
 
     return (
-        <div className="fixed z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl w-80 sm:w-96 h-fit flex flex-col gap-3 p-4 pl-6 pr-6 bg-[#E3F8ED] text-lg font-semibold border-[1px] border-solid border-green-300">
-            <div className="flex justify-between">
-                <h2 className="text-2xl font-bold mb-3 text-[#125C38]">
-                    Edit Transaction
-                </h2>
-                <button className="font-black font-mono text-2xl relative bottom-2" onClick={(e) => { e.stopPropagation(); closeModal() }}>X</button>
-            </div>
-            <div className="flex justify-between gap-[3.15rem] text-md mb-1">
-                <label className="self-center">Amount</label>
-                <input
-                    className="rounded-2xl w-1/2 h-9 text-black text-sm font-normal p-1 "
-                    type="number"
-                    value={amount}
-                    onChange={e => setAmount(e.target.value)}
-                />
-            </div>
+        <div onClick={(e) => e.stopPropagation()}>
+            <div className="fixed z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl w-80 sm:w-96 h-fit flex flex-col gap-3 p-4 pl-6 pr-6 bg-[#E3F8ED] text-lg font-semibold border-[1px] border-solid border-green-300">
+                <div className="flex justify-between">
+                    <h2 className="text-2xl font-bold mb-3 text-[#125C38]">
+                        Edit Transaction
+                    </h2>
+                    <button className="font-black font-mono text-2xl relative bottom-2" onClick={() => closeModal()}>X</button>
+                </div>
+                <div className="flex justify-between gap-[3.15rem] text-md mb-1">
+                    <label className="self-center">Amount</label>
+                    <input
+                        className="rounded-2xl w-1/2 h-9 text-black text-sm font-normal p-1 "
+                        type="number"
+                        value={amount}
+                        onChange={e => setAmount(e.target.value)}
+                    />
+                </div>
 
-            <div className="flex justify-between gap-[3.15rem] text-md mb-1">
-                <label className="self-center">Type</label>
-                <select className="rounded-2xl w-1/2 h-9 text-black text-sm font-normal p-1 bg-white "
-                    value={t_type}
-                    onChange={e => setType(e.target.value)}
-                >
-                    <option></option>
-                    <option value="expense">
-                        expense
-                    </option>
-                    <option value="income">
-                        income
-                    </option>
-                </select>
-            </div>
+                <div className="flex justify-between gap-[3.15rem] text-md mb-1">
+                    <label className="self-center">Type</label>
+                    <select className="rounded-2xl w-1/2 h-9 text-black text-sm font-normal p-1 bg-white "
+                        value={t_type}
+                        onChange={e => setType(e.target.value)}
+                    >
+                        <option></option>
+                        <option value="expense">
+                            expense
+                        </option>
+                        <option value="income">
+                            income
+                        </option>
+                    </select>
+                </div>
 
-            <div className="flex justify-between gap-[3.15rem] text-md mb-1">
-                <label className="self-center">Category</label>
-                <select className="rounded-2xl w-1/2 h-9 text-black text-sm font-normal p-1 bg-white "
-                    value={category}
-                    onChange={e => setCategory(e.target.value)}
-                >
-                    <option></option>
-                    <option value="food">
-                        food
-                    </option>
-                    <option value="clothing">
-                        clothing
-                    </option>
-                    <option value="rent">
-                        rent
-                    </option>
-                    <option value="entertainment">
-                        entertainment
-                    </option>
-                    <option value="investment">
-                        investment
-                    </option>
-                    <option value="transportation">
-                        transportation
-                    </option>
-                    <option value="borrowed">
-                        borrowed
-                    </option>
-                </select>
+                <div className="flex justify-between gap-[3.15rem] text-md mb-1">
+                    <label className="self-center">Category</label>
+                    <select className="rounded-2xl w-1/2 h-9 text-black text-sm font-normal p-1 bg-white "
+                        value={category}
+                        onChange={e => setCategory(e.target.value)}
+                    >
+                        <option></option>
+                        <option value="food">
+                            food
+                        </option>
+                        <option value="clothing">
+                            clothing
+                        </option>
+                        <option value="rent">
+                            rent
+                        </option>
+                        <option value="entertainment">
+                            entertainment
+                        </option>
+                        <option value="investment">
+                            investment
+                        </option>
+                        <option value="transportation">
+                            transportation
+                        </option>
+                        <option value="borrowed">
+                            borrowed
+                        </option>
+                    </select>
+                </div>
+                <div className="flex flex-col justify-between gap-4 text-md">
+                    <textarea
+                        className="rounded-2xl w-full max-h-48 text-black text-sm font-normal p-3"
+                        placeholder="Description"
+                        rows={10}
+                        value={t_description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                </div>
+                <button className="bg-[#125C38] w-20 rounded-lg p-1 text-white self-center mt-4 active:scale-95"
+                    onClick={async () => {
+                        const email = loggedInUser
+                        await handleClick({ id, email, amount, t_type, category, t_description, closeModal, setSyncTrigger })
+                        closeModal()
+                    }}>
+                    Save
+                </button>
             </div>
-            <div className="flex flex-col justify-between gap-4 text-md">
-                <textarea
-                    className="rounded-2xl w-full max-h-48 text-black text-sm font-normal p-3"
-                    placeholder="Description"
-                    rows={10}
-                    value={t_description}
-                    onChange={e => setDescription(e.target.value)}
-                />
-            </div>
-            <button className="bg-[#125C38] w-20 rounded-lg p-1 text-white self-center mt-4 active:scale-95"
-                onClick={async () => {
-                    const email = loggedInUser
-                    await handleClick({ id, email, amount, t_type, category, t_description, closeModal, setSyncTrigger })
-                }}>
-                Save
-            </button>
         </div>
     )
 }
